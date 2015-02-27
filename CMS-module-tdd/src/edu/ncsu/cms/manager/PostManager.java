6ab891340705640ebe1b293d0663b0261ac2a403
Prashant Trivedi <prashant.tri007@gmail.com>
2015-02-27 17:08:06 -0500

    Adding PostManager

---
+++ CMS-module-tdd/src/edu/ncsu/cms/manager/PostManager.java
@@ -0,0 +1,13 @@
+package edu.ncsu.cms.manager;
+import edu.ncsu.cms.model.*;
+
+public class PostManager {
+	
+		public static Post creatPost(){
+			return null;
+		}
+		
+		public static Post editPost(){
+			return null;
+		}
+}
