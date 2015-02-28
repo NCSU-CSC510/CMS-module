d91350c0a15f9360a948ed3cb47b8c0490a1610d
aashetty@ncsu.edu <aashetty@ncsu.edu>
2015-02-28 15:31:45 -0500

    Further Modifications to CreatePost story and steps file

--- CMS-Module-BDD/src/edu/ncsu/cms/specs/CreatePostSteps.java
+++ CMS-Module-BDD/src/edu/ncsu/cms/specs/CreatePostSteps.java
@@ -4,6 +4,30 @@ import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.jbehave.core.steps.Steps;
+import edu.ncsu.cms.model.*; 
 public class CreatePostSteps extends Steps{
-
+	Post postObj;
+	@Given("API call to the createPost method is made")
+	public void givenCreatePostCalled() {
+		//postObj=createPost();
+	}
+ 
+	@When("PostManager is invoked")
+	public void whenPostManagerInvoked() {
+		//postObj=PostManager.getPostObj;
+	}
+ 
+	
+	@Then("I see a new post created")
+	public void thenPostObjShouldBe() {
+		if (postObj==null)
+			throw new RuntimeException("new post Object not created!");
+	}
+	
+	@Then("version is assigned to $version")
+	public void thenVersionShouldBe(@Named("version") String version) {
+		//if (!version.equals("Draft"))
+		//	throw new RuntimeException("version is " + postObj.getVersion() + ", but should be " + version);
+	}
+	 
 }
