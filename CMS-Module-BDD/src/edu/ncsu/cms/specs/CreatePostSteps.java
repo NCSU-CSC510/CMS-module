3edb091d202edf5728bba626005132e6d46a2dd5
aashetty@ncsu.edu <aashetty@ncsu.edu>
2015-02-28 13:34:26 -0500

    Added specs package containing CreatePost story and Steps file

---
+++ CMS-Module-BDD/src/edu/ncsu/cms/specs/CreatePostSteps.java
@@ -0,0 +1,9 @@
+package edu.ncsu.cms.specs;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.jbehave.core.steps.Steps;
+public class CreatePostSteps extends Steps{
+
+}
