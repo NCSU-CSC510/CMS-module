a99b80b63637e3b122e0797c759c8bf14e393939
aashetty@ncsu.edu <aashetty@ncsu.edu>
2015-03-01 16:15:50 -0500

    Added PostManager along with functions to create a new post .

--- CMS-Module-BDD/src/edu/ncsu/cms/model/Post.java
+++ CMS-Module-BDD/src/edu/ncsu/cms/model/Post.java
@@ -4,7 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Post {
-
+	
 	private List<Version> versionList = new ArrayList<Version>();
 	private Version currentVersion;
+	private int postId;
+	private static int idCounter;
+	
+	Post(){
+		idCounter++;
+		currentVersion=new Version();
+		currentVersion.setState(State.DRAFT);
+		versionList.add(currentVersion);
+		postId=idCounter;
+	}
+	
+	public int getPostId(){
+	
+		return postId;
+	
+	}
+	public Version getCurrentVersion(){
+	
+		return currentVersion;
+	
+	}
+	public void updateVersion(String content){
+	
+		
+		//toDo
+	}
+	
+	
 }
---
+++ CMS-Module-BDD/src/edu/ncsu/cms/model/PostManager.java
@@ -0,0 +1,30 @@
+package edu.ncsu.cms.model;
+import java.util.*;
+
+
+public class PostManager {
+	
+	private HashMap<Integer,Post> postLists= new HashMap<Integer,Post>();
+	private static PostManager postMngrInstance=new PostManager();
+	private PostManager(){}
+	
+	public static PostManager instance(){
+		return postMngrInstance;
+	}
+	
+	public Post createNew(){
+		
+		Post p = new Post();
+		addPost(p);
+		return p;		
+	}
+	public void addPost(Post p){
+		postLists.put(p.getPostId(),p);
+	}
+	
+	public Post getPostById(int postID){
+		return postLists.get(postID);
+	}
+	
+	
+}
