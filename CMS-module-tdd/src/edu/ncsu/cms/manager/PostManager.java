1566b064957ec5b83498a709ef2217ab005c2b02
Prashant Trivedi <prashant.tri007@gmail.com>
2015-03-21 01:00:28 -0400

    Edit post related code changes

--- CMS-module-tdd/src/edu/ncsu/cms/manager/PostManager.java
+++ CMS-module-tdd/src/edu/ncsu/cms/manager/PostManager.java
@@ -6,9 +6,9 @@ import edu.ncsu.cms.model.*;
 public class PostManager {
 
 	private HashMap<Integer,Post> postsMap = new HashMap<Integer,Post>();
-	
+
 	public Post createPost(){
-		
+
 		Post newPost = new Post();
 		postsMap.put(newPost.getPostID(), newPost);
 		return newPost;		
@@ -16,7 +16,7 @@ public class PostManager {
 	public void addPostToList(Post p){
 		postsMap.put(p.getPostID(),p);
 	}
-	
+
 	public Post getPostById(int postID){
 		return postsMap.get(postID);
 	}
@@ -24,18 +24,28 @@ public class PostManager {
 		Post post = postsMap.get(postID);
 		return post.getPostVersion(post, versionid);
 	}
-	
+
 	public Version editPostVersion(Post p, int VersionID) {
 		Version ver = getPostVersionById(p.getPostID(), VersionID);
-		ver.setState(State.DRAFT);
-		return ver;
+		if(ver.getState()!=State.DRAFT){
+			Version newVer = new Version();
+			newVer.setState(State.DRAFT);
+			newVer.setVersionId(ver.getVersionId()+1);
+			p.getVersionList().add(newVer);
+			return newVer;
+		}else{
+			return ver;
+		}
 	}
-	
+
 	public void deletePost(int postID){
 		Post p = getPostById(postID);
 		if(p!=null){
 			postsMap.remove(p);
-		}
-		
+		} 
+	}
+	public void publishPostVersion(Post post, int versionID){
+		Version ver = post.getPostVersion(post, versionID);
+		ver.setState(State.PUBLISHED);
 	}
 }
--- CMS-module-tdd/src/edu/ncsu/cms/test/model/PostTest.java
+++ CMS-module-tdd/src/edu/ncsu/cms/test/model/PostTest.java
@@ -24,4 +24,24 @@ public class PostTest {
 		postVer.setContent(content);
 		assertEquals("Content of post doesn not match", content, postVer.getContent());
 	}
+	
+	@Test
+	public void testEditPost(){
+		PostManager mgr = new PostManager();
+		Post newPost = mgr.createPost();
+		assertNotNull(newPost);
+		Version postVer = newPost.getCurrentVersion();
+		assertNotNull(postVer);
+		assertEquals("Post must be in draft State", State.DRAFT, postVer.getState());
+		String content = "This is my first Post";
+		postVer.setContent(content);
+		assertEquals("Content of post doesn not match", content, postVer.getContent());
+		mgr.publishPostVersion(newPost, postVer.getVersionId());
+		assertEquals("Post version Not published",State.PUBLISHED, postVer.getState());
+		Version newVer = mgr.editPostVersion(newPost,postVer.getVersionId());
+		assertEquals("The value of edit post version doesn't match",2, newVer.getVersionId());
+		String newContent = "Post Edited";
+		newVer.setContent(newContent);
+		assertEquals("Edited post version text doesn't match",newContent, newVer.getContent());
+	}
 }
